1b8562c3415bb4e1797b2ab3eb819c414944e16f
Lawrence Liu <xqliu@me.com>
2012-12-16 19:51:03 +0800

    Finish task BGA-25, on ripple, not sure whether works on simulator

--- js/task-list.js
+++ js/task-list.js
@@ -1,32 +1,12 @@
-﻿//function dialogCallBack(index){
-//alert("I am here");
-//}
-//function customDialog() {
-//try {
-//var buttons = ["Yes", "No", "Sometimes", "NA"];
-//var ops = {title : "Choose the answer that describes you best", size : "tall", position : "middleCenter"};
-//blackberry.ui.dialog.customAskAsync("Do you routinely work out?", buttons, dialogCallBack, ops);
-//} catch(e) {
-//alert("Exception in customDialog: " + e);
-//}
-//}
-
-function showCustomToast() {
-    alert("I am an alert");
-    //var message = "This is my toast!",
-    //buttonText = "Click Me",
-    //toastId,
-    //onButtonSelected = function () {
-        //console.log('Button was clicked for toast: ' + toastId);
-    //},
-    //onToastDismissed = function () {
-        //console.log('Toast disappeared: ' + toastId);
-    //},
-    //options = {
-        //buttonText : buttonText,
-        //dismissCallback : onToastDismissed,
-        //buttonCallback : onButtonSelected
-    //};
-
-    //toastId = blackberry.ui.toast.show(message, options);
+﻿function dialogCallBack(index){
+    alert(index);
+}
+function customDialog(task_title) {
+    try {
+        var buttons = ["Done!", "Postpone :(", "Open Task"];
+            var ops = {title : "Peaceful & Better Life's Reminder", size : "large", position : "middleCenter"};
+            blackberry.ui.dialog.customAskAsync(task_title, buttons, dialogCallBack, ops);
+    } catch(e) {
+        console.log("Exception in customDialog: " + e);
+    }
 }
