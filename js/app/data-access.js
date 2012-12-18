ef02d9c691e47b8c8085bfeaecfdfd3cd6e55b56
Lawrence Liu <xqliu@me.com>
2012-12-19 02:35:55 +0800

    Finish task BGA-26 and corresponding unit test script

--- js/app/data-access.js
+++ js/app/data-access.js
@@ -1,4 +1,17 @@
-﻿var dataAccess = (function (){
+﻿var db_init_sql_script = [
+    'CREATE TABLE IF NOT EXISTS task (id int unique, title text);',
+    'CREATE TABLE IF NOT EXISTS task_meta (id int unique, task_id int, meta_id int);',
+    'CREATE TABLE IF NOT EXISTS meta (id int unique, type_id int, name text, description text);',
+    'CREATE TABLE IF NOT EXISTS meta_type (id int unique, name text, description text);',
+    'CREATE TABLE IF NOT EXISTS task_note (id int unique, task_id int, content text, create_date real);',
+    'CREATE TABLE IF NOT EXISTS task_reminder (id int unique, task_id int, next_reminder_time real);' 
+];
+
+var dataAccess = (function (){
+    var dbInited = false,
+    initdb = function(){
+        html5sql.process(db_init_sql_script);
+    };
     return {
         logInfo: false,
         logErrors: false,
@@ -6,15 +19,9 @@
             if(typeof(html5sql.database) === null){
                 html5sql.openDatabase('peaceful_better_life_xiangqian_liu', '0.0.1', 'Local Database for Peaceful & Better Life App', 2*1024*1024);
             }
+            initdb();            
             return html5sql.database;
         },
     };
 })();
-/*
-*tx.executeSql('CREATE TABLE task (id int unique, title text);'  
-*+ 'CREATE TABLE task_meta (id int unique, task_id int, meta_id int);'  
-*+ 'CREATE TABLE meta (id int unique, type_id int, name text, description text);' 
-*+ 'CREATE TABLE meta_type (id int unique, name text, description text);' 
-*+ 'CREATE TABLE task_note (id int unique, task_id int, content text, create_date real);'
-*+ 'CREATE TABLE task_reminder (id int unique, task_id int, next_reminder_time real)'),
-*/
+
--- js/spec/data-access-spec.js
+++ js/spec/data-access-spec.js
@@ -1,16 +1,67 @@
 describe("Data Access API", function() {
 
-  beforeEach(function() {
-  });
+    var db;
+    var check_table_exists_sql = "SELECT COUNT(*) FROM sqlite_master where type='table' and name='?'";
+    var table_names = [{
+        task: 'task',
+        task_meta: 'task_meta',
+        meta: 'meta',
+        meta_type: 'meta_type',
+        task_note: 'task_note',
+        task_reminder: 'task_reminder'
+    }];
 
-  it("This is just a hello world unit test case to make sure the unit test framework is working!", function() {
+    beforeEach(function() {
+        db = dataAccess.openDatabase();
+    });
+
+    it("Just a hello world unit test case to make sure the unit test framework is working!", function() {
         var helloworld="Hello World!";
         expect("Hello World!").toEqual(helloworld);
-  });
+    });
 
-  it("The database opened should be defined", function(){
-      var db = dataAccess.openDatabase();
-      expect(db).toBeDefined();
-  });
+    it("The database opened should be defined", function(){
+        expect(db).toBeDefined();
+    });
+
+    describe("Upon opening db, all tables should be exists", function(){
+
+        function check_table_exists (table_name){
+            html5sql.process(
+                check_table_exists_sql,
+                table_name,
+                function(transaction, results){
+                    expect(1).toEqual(results);
+                },
+                function(error, statement){
+                    expect(true).toBeFalsy();                
+                }
+            );
+        };
+
+        it("task table should be created"), function(){
+            check_table_exists(table_names.task);
+        };
+
+        it("task_meta table should be created"), function(){
+            check_table_exists(table_names.task_meta);
+        };
+
+        it("meta table should be created"), function(){
+            check_table_exists(table_names.meta);
+        };
+
+        it("meta_type table should be created"), function(){
+            check_table_exists(table_names.meta_type);
+        };
+
+        it("task_note table should be created"), function(){
+            check_table_exists(table_names.task_note);
+        };
+
+        it("task_reminder table should be created"), function(){
+            check_table_exists(table_names.task_reminder);
+        };
+    });
 
 });
