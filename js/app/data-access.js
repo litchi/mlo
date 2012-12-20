e666fe2ac58f7a16344e68f27ad5f9ab26a78400
Lawrence Liu <xqliu@me.com>
2012-12-20 09:16:16 +0800

    Work on BGA-28 and fix testing issues of BGA-26

--- js/app/data-access.js
+++ js/app/data-access.js
@@ -1,26 +1,59 @@
-﻿var db_init_sql_script = [
-    'CREATE TABLE IF NOT EXISTS task (id int unique, title text);',
-    'CREATE TABLE IF NOT EXISTS task_meta (id int unique, task_id int, meta_id int);',
-    'CREATE TABLE IF NOT EXISTS meta (id int unique, type_id int, name text, description text);',
-    'CREATE TABLE IF NOT EXISTS meta_type (id int unique, name text, description text);',
-    'CREATE TABLE IF NOT EXISTS task_note (id int unique, task_id int, content text, create_date real);',
-    'CREATE TABLE IF NOT EXISTS task_reminder (id int unique, task_id int, next_reminder_time real);' 
+﻿var DB_INIT_SQL = [
+    'CREATE TABLE IF NOT EXISTS task (id INTEGER PRIMARY KEY AUTOINCREMENT, title text);',
+    'CREATE TABLE IF NOT EXISTS task_meta (id INTEGER PRIMARY KEY AUTOINCREMENT, task_id int, meta_id int);',
+    'CREATE TABLE IF NOT EXISTS meta (id INTEGER PRIMARY KEY AUTOINCREMENT, type_id int, name text, description text);',
+    'CREATE TABLE IF NOT EXISTS meta_type (id INTEGER PRIMARY KEY AUTOINCREMENT, name text, description text);',
+    'CREATE TABLE IF NOT EXISTS task_note (id INTEGER PRIMARY KEY AUTOINCREMENT, task_id int, content text, create_date real);',
+    'CREATE TABLE IF NOT EXISTS task_reminder (id INTEGER PRIMARY KEY AUTOINCREMENT, task_id int, next_reminder_time real);' 
 ];
 
+var INSERT_SQL = {
+   task : 'insert into task (id, title) values (null, ?)',
+   meta_type : 'insert into meta_type (id, name, description) values (null, ?, ?)',
+   meta : 'insert into task meta (id, type_id, name, description) values (null, ?, ?, ?)',
+   task_meta : 'insert into task_meta (id, type_id, name, description) values (null, ?, ?, ?)',
+   task_note : 'insert into task_note (id, task_id, content, create_date) values (null, ?, ?, ?)',
+   task_reminder : 'insert into task_reminder (id, task_id, next_reminder_time) values (null, ?, ?)'
+};
+
 var dataAccess = (function (){
-    var dbInited = false,
-    initdb = function(){
-        html5sql.process(db_init_sql_script);
-    };
+    var dbInited = false;
+
+    function lazyOpenDatabase(){
+        if(false == dbInited) {
+            dataAccess.openDatabase();
+        }
+    }
+
+    function runSQL(sql, data, successCallback, failureCallback){
+        html5sql.process(
+            [{
+                sql: sql,
+                data: [data]
+            }],
+            successCallback,
+            failureCallback
+        );
+    }
+
     return {
         logInfo: false,
-        logErrors: false,
+        logErrors: true,
         openDatabase: function (){
-            if(typeof(html5sql.database) === null){
-                html5sql.openDatabase('peaceful_better_life_xiangqian_liu', '0.0.1', 'Local Database for Peaceful & Better Life App', 2*1024*1024);
+            if(html5sql.database === null){
+                html5sql.openDatabase('peaceful_better_life_xiangqian_liu', 'Local Database for Peaceful & Better Life App', 2*1024*1024);
             }
-            initdb();            
-            return html5sql.database;
+            html5sql.changeVersion("0.0.2", "0.0.3", DB_INIT_SQL);
+            var result = html5sql.database;
+            if(result != null){
+                dbInited = true;
+            }
+            return result;
+        },
+                                                   
+        createTask: function(title, successCallback, failureCallback){
+            lazyOpenDatabase();
+            runSQL(INSERT_SQL.task, [title], successCallback, failureCallback);
         },
     };
 })();
--- js/spec/data-access-spec.js
+++ js/spec/data-access-spec.js
@@ -1,15 +1,16 @@
 describe("Data Access API", function() {
 
     var db;
-    var check_table_exists_sql = "SELECT COUNT(*) FROM sqlite_master where type='table' and name='?'";
-    var table_names = [{
+    var check_table_exists_sql = "SELECT COUNT(*) FROM sqlite_master where type='table' and name=?";
+    var table_names = {
         task: 'task',
         task_meta: 'task_meta',
         meta: 'meta',
         meta_type: 'meta_type',
         task_note: 'task_note',
         task_reminder: 'task_reminder'
-    }];
+    };
+    var task_table_insert_sql = "insert into task"; 
 
     beforeEach(function() {
         db = dataAccess.openDatabase();
@@ -17,51 +18,94 @@ describe("Data Access API", function() {
 
     it("Just a hello world unit test case to make sure the unit test framework is working!", function() {
         var helloworld="Hello World!";
-        expect("Hello World!").toEqual(helloworld);
+        expect(helloworld).toEqual("Hello World!");
     });
 
-    it("The database opened should be defined", function(){
+    it("The database should be opened successfully", function(){
         expect(db).toBeDefined();
+        var notNull = (db == null);
+        expect(notNull).toBeFalsy();
     });
 
     describe("Upon opening db, all tables should be exists", function(){
 
         function check_table_exists (table_name){
+            console.log("Checking table '" + table_name + "' exists");
+            var t_results, t_arrays, t_error;
             html5sql.process(
-                check_table_exists_sql,
-                table_name,
-                function(transaction, results){
-                    expect(1).toEqual(results);
+                [{
+                    sql: check_table_exists_sql,
+                    data: [table_name]
+                }],
+                function(transaction, results, arrays){
+                    t_results = results;
+                    t_arrays = arrays;
                 },
                 function(error, statement){
-                    expect(true).toBeFalsy();                
+                    t_error = error;
                 }
             );
+            waits(100);
+            runs(function() {
+                expect(t_error).toBeUndefined();
+                expect(t_arrays).toBeDefined();
+                expect(t_arrays.length).toEqual(1);
+                expect(t_arrays[0]["COUNT(*)"]).toEqual(1);
+            });
         };
 
-        it("task table should be created"), function(){
+        it("task table should exists", function(){
             check_table_exists(table_names.task);
-        };
+        });
 
-        it("task_meta table should be created"), function(){
+        it("task_meta table should be created", function(){
             check_table_exists(table_names.task_meta);
-        };
+        });
 
-        it("meta table should be created"), function(){
+        it("meta table should be created", function(){
             check_table_exists(table_names.meta);
-        };
+        });
 
-        it("meta_type table should be created"), function(){
+        it("meta_type table should be created", function(){
             check_table_exists(table_names.meta_type);
-        };
+        });
 
-        it("task_note table should be created"), function(){
+        it("task_note table should be created", function(){
             check_table_exists(table_names.task_note);
-        };
+        });
 
-        it("task_reminder table should be created"), function(){
+        it("task_reminder table should be created", function(){
             check_table_exists(table_names.task_reminder);
-        };
+        });
     });
+    
+    describe("Insert into tables", function(){
+        it("Insert into task table", function(){
 
+            var taskTitle = "This is the first task";
+
+            dataAccess.createTask(taskTitle);
+
+            var t_results, t_arrays, t_error;
+            html5sql.process(
+                [{
+                    sql: "select id, title from task where title = ?",
+                    data: [taskTitle]
+                }],
+                function(transaction, results, arrays){
+                    t_results = results;
+                    t_arrays = arrays;
+                },
+                function(error, statement){
+                    t_error = error;
+                }
+            );
+            waits(100);
+            runs(function() {
+                expect(t_error).toBeUndefined();
+                expect(t_arrays).toBeDefined();
+                expect(t_arrays.length).toEqual(1);
+            });
+        });
+    });
 });
