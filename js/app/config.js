8c9a1f6f656aa0e3b55bb8cb25ef5fc3739c5484
Lawrence Liu <xqliu@me.com>
2012-12-22 23:09:51 +0800

    Add a config.js for application configurations

---
+++ js/app/config.js
@@ -0,0 +1,10 @@
+var uiConfig = (function(){
+
+    return {
+      leftPanelWidth  : '32%',
+      rightPanelWidth : '70%',
+      rightPanelSmallerLeftMargin : '240',
+      rightPanelLargerLeftMargin : '520'
+    };
+
+})(); 
