8c2c39d4e79db082af793b47c56f899255a19c16
Lawrence Liu <xqliu@me.com>
2013-01-05 23:15:36 +0800

    Add development menu for easier development on simulator and device

---
+++ js/app/development.js
@@ -0,0 +1,28 @@
+dataAccess.createDatabaseConnection();
+
+function reCreateDatabase(){
+    dataAccess.appDb.transaction(function(tx){
+        dataAccess.dropAllTables(tx);
+    }, function(error){
+        console.error("Error to drop all tables, error code " + error.code + ", error msg: " + error.message);
+    }, function(){
+        dataAccess.initAppDb(dataAccess.appDb);
+        console.debug("Successfully drop and recreate all tables and import seed data");
+    });
+}
+
+function printTable(tableName){
+    console.debug("\n-------------------------------- " + tableName + " --------------------------------"); 
+    dataAccess.appDb.transaction(function(tx){
+        tx.executeSql('select * from ' + tableName, [], function(t, r){
+            var max;
+            for(i = 0, max = r.rows.length; i < max; i++){
+                obj = r.rows.item(i);
+                console.debug(obj);
+            }
+            console.debug("-------------------------------- " + tableName + " --------------------------------\n"); 
+        }, function(t, e){
+            console.error("Failed to get data of table " + tableName + "Error code: " + e.code + ", Error msg: " + e.message);
+        });
+    })
+}
