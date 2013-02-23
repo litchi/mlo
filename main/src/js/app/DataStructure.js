13aa291495411b28e52efa42765a1ac208101f50
Lawrence as Developer <smartlitchi@gmail.com>
2013-02-24 02:47:02 +0800

    Resolve issue BGA-182 (Tasks in trash was not filtered out when in Due page.)

--- main/src/js/app/DataStructure.js
+++ main/src/js/app/DataStructure.js
@@ -136,14 +136,14 @@ Sql = {
         DueFilterBaseSql : 'select id, name, due_date from task where %DueFilter% order by due_date',
         DueFilterKey     : 'DueFilter',
         DueFilter : {
-            'All'               : "due_date is not null and status != '" + SeedData.TaskDoneStatus + "'",
-            'Today'             : "strftime('%Y-%m-%d', due_date, 'unixepoch') = date('now') and status != '" + SeedData.TaskDoneStatus + "'",
-            'Tomorrow'          : "strftime('%Y-%m-%d', due_date, 'unixepoch') = date('now','+1 day') and status != '" + SeedData.TaskDoneStatus + "'",
-            'This Week'         : "strftime('%Y-%W', due_date, 'unixepoch') = strftime('%Y-%W', 'now') and status != '" + SeedData.TaskDoneStatus + "'",
-            'Next Week'         : "strftime('%Y-%W', due_date, 'unixepoch') = strftime('%Y-%W', 'now', '+7 days') and status != '" + SeedData.TaskDoneStatus + "'",
-            'Done Yesterday'    : "strftime('%Y-%m-%d', due_date, 'unixepoch') = date('now','-1 day') and status = '" + SeedData.TaskDoneStatus + "'",
-            'Overdue Yesterday' : "strftime('%Y-%m-%d', due_date, 'unixepoch') = date('now','-1 day') and status != '" + SeedData.TaskDoneStatus + "'",
-            'Overdue'           : "strftime('%Y-%m-%d %H:%M:%S', due_date, 'unixepoch') < datetime('now') and status != '" + SeedData.TaskDoneStatus + "'"
+            'All'               : "due_date is not null and status != '" + SeedData.TaskDoneStatus + "' and status != '" + SeedData.TaskDeletedStatus + "'",
+            'Today'             : "strftime('%Y-%m-%d', due_date, 'unixepoch') = date('now') and status != '" + SeedData.TaskDoneStatus + "' and status != '" + SeedData.TaskDeletedStatus + "'", 
+            'Tomorrow'          : "strftime('%Y-%m-%d', due_date, 'unixepoch') = date('now','+1 day') and status != '" + SeedData.TaskDoneStatus + "' and status != '" + SeedData.TaskDeletedStatus + "'",
+            'This Week'         : "strftime('%Y-%W', due_date, 'unixepoch') = strftime('%Y-%W', 'now') and status != '" + SeedData.TaskDoneStatus + "' and status != '" + SeedData.TaskDeletedStatus + "'",
+            'Next Week'         : "strftime('%Y-%W', due_date, 'unixepoch') = strftime('%Y-%W', 'now', '+7 days') and status != '" + SeedData.TaskDoneStatus + "' and status != '" + SeedData.TaskDeletedStatus + "'",
+            'Done Yesterday'    : "strftime('%Y-%m-%d', due_date, 'unixepoch') = date('now','-1 day') and status = '" + SeedData.TaskDoneStatus + "' and status != '" + SeedData.TaskDeletedStatus + "'",
+            'Overdue Yesterday' : "strftime('%Y-%m-%d', due_date, 'unixepoch') = date('now','-1 day') and status != '" + SeedData.TaskDoneStatus + "' and status != '" + SeedData.TaskDeletedStatus + "'",
+            'Overdue'           : "strftime('%Y-%m-%d %H:%M:%S', due_date, 'unixepoch') < datetime('now') and status != '" + SeedData.TaskDoneStatus + "' and status != '" + SeedData.TaskDeletedStatus + "'"
         }
     }
 };
